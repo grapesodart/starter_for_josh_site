47c201d027920edfd8edf95d76be8e535d88fc36
grapesodart <jacobeverest@yahoo.com.au>
2020-04-29 23:11:02 +0800

    create home-page - take 2

---
+++ home-page.php
@@ -0,0 +1,61 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+
+    <section class="container-fluid aboutbg text-center">
+        <!-- about me section container, goes to edge-->
+
+        <div class="container">
+           
+            
+               <!-- Placeholder for aboutme -->
+            
+             
+ <div class="blocker"></div>
+
+
+
+        </div> <!-- end of about me section content -->
+    </section> <!-- end of about me section -->
+
+
+    <section class="container-fluid articlebg">
+        <!-- articles container, goes to edge -->
+        <div class="container">
+            <!-- articles content container -->
+            <div class="row">
+                <!-- row for BS columns -->
+
+                <div>
+                    <!-- article section header -->
+                    <h3 class="text-center article-title">LATEST ARTICLES</h3>
+                </div>
+
+                <div class="col-md-4 article-cent">
+                    
+                    <!-- col1 placeholder -->
+                
+                </div> <!-- end of article -->
+
+                <div class="col-md-4 article-cent">
+
+                    <!-- col2 placeholder -->
+                    
+                </div> <!-- end of article -->
+
+                <div class="col-md-4 article-cent">
+
+                    <!-- col3 placeholder -->
+                    
+                </div> <!-- end of article -->
+            </div><!-- end of row -->
+
+
+        </div>
+
+
+    </section> <!-- end of articles section -->
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
