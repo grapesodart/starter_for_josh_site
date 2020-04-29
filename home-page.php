6f45ad4b73b934f4820061e328680d8fea406c69
grapesodart <jacobeverest@yahoo.com.au>
2020-04-29 21:32:45 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,61 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+
+    <section class="container-fluid aboutbg text-center">
+        <!-- about me section container, goes to edge-->
+
+        <div class="container">
+            <!-- second section content -->
+            <h2> GAMES &amp; LEVEL DESIGNER</h2>
+            <p class="particle">...and particle wizard</p>
+            <p class="about-text">My name is Josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments. I always tell a story.</p>
+            <div class="blocker"></div>
+
+        </div> <!-- end of about me section content -->
+    </section> <!-- end of about me section -->
+
+
+    <section class="container-fluid articlebg">
+        <!-- articles container, goes to edge -->
+        <div class="container">
+            <!-- articles content container -->
+            <div class="row">
+                <!-- row for BS columns -->
+
+                <div>
+                    <!-- article section header -->
+                    <h3 class="text-center article-title">LATEST ARTICLES</h3>
+                </div>
+
+                <div class="col-md-4 article-cent">
+                    
+        <!-- placeholder for col1 -->
+                    
+                    
+                </div> <!-- end of article -->
+
+                <div class="col-md-4 article-cent">
+                    
+        <!-- placeholder for col2 -->
+                    
+                    
+                </div> <!-- end of article -->
+
+                <div class="col-md-4 article-cent">
+                    
+        <!-- placeholder for col3 -->
+                    
+                    
+                </div> <!-- end of article -->
+            </div><!-- end of row -->
+
+
+        </div>
+
+
+    </section> <!-- end of articles section -->
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
