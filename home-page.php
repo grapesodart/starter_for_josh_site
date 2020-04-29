aa17128c793a335c7b1e9b48419c36bf0c8c5e25
grapesodart <jacobeverest@yahoo.com.au>
2020-04-29 23:39:37 +0800

    article update for home-page
    including a nifty alt text grabber for article titles

--- home-page.php
+++ home-page.php
@@ -4,58 +4,63 @@
     */
 ?>
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
-
-    <section class="container-fluid aboutbg text-center">
-        <!-- about me section container, goes to edge-->
-
-        <div class="container">
-           
-            
-               <!-- Placeholder for aboutme -->
-            
-             
- <div class="blocker"></div>
+<section class="container-fluid aboutbg text-center">
+    <div class="container">
 
 
+        <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+        <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+        <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
 
-        </div> <!-- end of about me section content -->
-    </section> <!-- end of about me section -->
+
+        <div class="blocker"></div>
+    </div><!--  container-->
+</section>
 
 
-    <section class="container-fluid articlebg">
-        <!-- articles container, goes to edge -->
-        <div class="container">
-            <!-- articles content container -->
-            <div class="row">
-                <!-- row for BS columns -->
+<section class="container-fluid articlebg">
+    <!-- articles container, goes to edge -->
+    <div class="container">
+        <!-- articles content container -->
+        <div class="row">
+            <!-- row for BS columns -->
 
-                <div>
-                    <!-- article section header -->
-                    <h3 class="text-center article-title">LATEST ARTICLES</h3>
-                </div>
+            <div>
+                <h3 class="text-center article-title"><?php the_field('titleforarticles'); ?></h3>
+            </div>
 
-                <div class="col-md-4 article-cent">
-                    
-                    <!-- col1 placeholder -->
-                
-                </div> <!-- end of article -->
 
-                <div class="col-md-4 article-cent">
+            <div class="col-md-4 article-cent">
 
-                    <!-- col2 placeholder -->
-                    
-                </div> <!-- end of article -->
+                <img class="img-responsive" alt="<?php the_field('articletitle'); ?>" src="<?php the_field('articleimage'); ?>">
+                <h4><?php the_field('articletitle'); ?></h4>
+                <p><?php the_field('articleexcerpt'); ?> </p>
+                <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
 
-                <div class="col-md-4 article-cent">
 
-                    <!-- col3 placeholder -->
-                    
-                </div> <!-- end of article -->
-            </div><!-- end of row -->
+            </div>
+
+            <div class="col-md-4 article-cent">
+
+                <img class="img-responsive" alt="<?php the_field('articletitle1'); ?>" src="<?php the_field('articleimage1'); ?>">
+                <h4><?php the_field('articletitle1'); ?></h4>
+                <p><?php the_field('articleexcerpt1'); ?> </p>
+                <a href="<?php the_field('readmore1'); ?>" class="readmore">CONTINUE READING</a>
+
+
+            </div>
+            <div class="col-md-4 article-cent">
+
+                <img class="img-responsive" alt="<?php the_field('articletitle2'); ?>" src="<?php the_field('articleimage2'); ?>">
+                <h4><?php the_field('articletitle2'); ?></h4>
+                <p><?php the_field('articleexcerpt2'); ?> </p>
+                <a href="<?php the_field('readmore2'); ?>" class="readmore">CONTINUE READING</a>
+
+            </div>
 
 
         </div>
 
 
-    </section> <!-- end of articles section -->
+</section> <!-- end of articles section -->
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
